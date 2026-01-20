e50eeca50a032c3c21f00da3eb6580671f430d3c
fernandosstic <fernando.sersan.2@educa.jcyl.es>
2026-01-20 11:01:40 +0100

    Add files via upload

---
+++ primerjs.js
@@ -0,0 +1,5 @@
+var x=parseFloat(prompt("Introduce un número"));
+var t=parseFloat(prompt("Introduce un número"));
+var z=parseFloat(prompt("Introduce un número"));
+var resultado=x*z*t;
+document.write("EL resultado de multiplicar estos números es " + resultado );
